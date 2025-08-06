16e3480f82a0619b46f336a224ebfcaace6ae3a2
Crealex <Alexandre.tomasi80@gmail.com>
2025-08-07 00:08:28 +0200

    add a comment for remember a thing for method what()

--- ex00/GradeToLowExecption.cpp
+++ ex00/GradeToLowExecption.cpp
@@ -2,7 +2,7 @@
 #include "colors.h"
 #include "GradeToLowExecption.hpp"
 
-char const *GradeToLowExecption::what()
+char const *GradeToLowExecption::what() // maybe "throe (std::exception & e)"
 {
 	return ("Grade to Low!");
 }
